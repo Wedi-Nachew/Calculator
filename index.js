6cfbdc369280b8e6e0964b1c7983c221406611e7
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-17 10:11:39 +0300

    Add border to the calculator

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let a;
-let b;
+let a = number();
+let b = number();
 let operator;
 
 function add(a,b){
@@ -14,9 +14,42 @@ function multiply(a,b){
 function divid(a,b){
     return a/b
 }
+function number(select){
+    if(select ==1){
+    num = 1;
+    a = num;
+    b = num;
+    console.log( add(a,b))
+    }else if(select ==2){
+       return num = 2
+    }else if(select ==3){
+       return  num = 3
+    }else if(select ==4){
+       return num = 4
+    }else if(select ==5){
+        return num = 5
+    }else if(select ==6){
+       return num = 6
+    }else if(select ==7){
+        return num = 7
+    }else if(select ==8){
+       return num = 8
+    }else if(select ==9){
+        return num = 9
+    }else if(select ===0){
+        r
+    }
+
+}
+console.log(number())
 function operate(a,operate,b){
     add(a,b)
     subtract(a,b)
     multiply(a,b)
     divid(a,b)
 }
+function renderNumbers(){
+
+}
+
+const display = renderNumbers()
