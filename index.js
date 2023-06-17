838e3843de49dcbdc1353773c00ffda1df9f5bde
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-17 09:08:38 +0300

    Create function for the basic maths operations

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+} 
+function multiply(a,b){
+    return a*b 
+}
+function divid(a,b){
+    return a/b
+}
+console.log(add(4,5))
+console.log(subtract(3,10))
+console.log(multiply(4,90))
+console.log(divid(7,3))
