b7dcaaeefd6d76cc79e972a1c7bab33fa45d624c
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-18 16:05:06 +0300

    Round the answer to four decimal points

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ let count = 0;
 let result = 0;
 
 function add(firstNumber,secondNumber){
-    return (~~firstNumber + ~~secondNumber)
+    return Math.round((~~firstNumber + ~~secondNumber) * 10000)/10000
 }
 function subtract(firstNumber,secondNumber){
-    return (~~firstNumber - ~~secondNumber)
+    return Math.round((~~firstNumber - ~~secondNumber) *10000)/ 10000
 } 
 function multiply(firstNumber,secondNumber){
-    return (~~firstNumber * ~~secondNumber)
+    return Math.round((~~firstNumber * ~~secondNumber) *10000)/10000
 }
 function divide(firstNumber,secondNumber){
-    return (~~firstNumber / ~~secondNumber)
+    return Math.round((~~firstNumber / ~~secondNumber) *10000)/10000
 }
 function operate(operator,firstNumber,secondNumber){
     if (operator == '+'){
@@ -64,90 +64,3 @@ equalsTo.addEventListener("click", ()=> {
     result = operate(operator,firstNumber,secondNumber);
     showNumbers.textContent = result;
 })
-
-/*getNumber.forEach(item => item.addEventListener("click", (e)=> { 
-    if(!operator && !firstNumber) {
-        firstNumber = e.target.dataset.value;
-        showNumbers.textContent = firstNumber;
-    } else if (!operator && firstNumber){
-        firstNumber += e.target.dataset.value;
-        showNumbers.textContent = firstNumber;
-    } else if (count && result && secondNumber && 
-        (e.target.dataset.value == '*' || (e.target.dataset.value == '/'))){
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    }else if (count && result && secondNumber && 
-        (e.target.dataset.value == '+' || (e.target.dataset.value == '-'))){
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    }else if (operator && !secondNumber) {
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    } else if (operator && secondNumber) {
-        secondNumber += e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    } else if( count && result && !secondNumber) {
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    } else if (count && result && secondNumber) {
-        secondNumber += e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-    } 
-    else if(count && !result && secondNumber) {
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-     } else if (count && !result && !secondNumber){
-        secondNumber = e.target.dataset.value;
-        showNumbers.textContent = secondNumber;
-     }
-
-})) ;*/
-/* operators.forEach(item => item.addEventListener("click", (e)=> {
-    if (!count) {
-        operator = e.target.dataset.value;
-        count++
-    }else if(count && !result ) {
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        result = firstNumber;
-        secondNumber = 0;
-        showNumbers.textContent = firstNumber;
-        operator= e.target.dataset.value;
-    }
-    else if( count && result && !secondNumber){
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        showNumbers.textContent = firstNumber;
-        operator = e.target.dataset.value;
-    }
-     else if (count && result && secondNumber && 
-        (e.target.dataset.value == '+' || (e.target.dataset.value == '-'))) {
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        showNumbers.textContent = firstNumber;
-        operator= e.target.dataset.value;
-        result = 0;
-        secondNumber = 0;
-    } else if (count && result && secondNumber && 
-        (e.target.dataset.value == '*' || (e.target.dataset.value == '/'))) {
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        showNumbers.textContent = firstNumber;
-        operator= e.target.dataset.value;
-        result = 0;
-        secondNumber = 1;
-    } else if (count && !result && secondNumber && 
-        (e.target.dataset.value == '*' || (e.target.dataset.value == '/'))) {
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        showNumbers.textContent = firstNumber;
-        operator= e.target.dataset.value;
-        result = 0;
-        secondNumber = 1;
-    }else if (count && !result && !secondNumber && 
-        (e.target.dataset.value == '+' || (e.target.dataset.value == '-'))) {
-        firstNumber = operate(operator,firstNumber,secondNumber);
-        showNumbers.textContent = firstNumber;
-        operator= e.target.dataset.value;
-        result = 0;
-        secondNumber = 0;
-    }
-    
-    
-})) */
-
