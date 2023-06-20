2894929dca28e24bab938c908b75e390dd6d5efe
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-20 09:12:42 +0300

    Display special symbols correctly

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ let count = 0;
 let result = 0;
 const clear = document.querySelector("#clear")
 const floatPoint = document.querySelector("#point")
-const sqr = document.querySelector(".sqr")
-sqr.textContent = "x ** 2"
 
 
 function add(firstNumber,secondNumber){
