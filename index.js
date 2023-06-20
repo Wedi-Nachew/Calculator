5ab04e9d7f94ee8e6bc64c708339d03f7a310a81
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-20 11:30:03 +0300

    Create functions for the additional operations

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ function operate(operator,firstNumber,secondNumber){
         return divide(firstNumber,secondNumber)
     }
 }
+function power(firstNumber,secondNumber){
+    return (firstNumber ** secondNumber);
+}
+
+/*function sqaurRoot(number){
+    return (number ** 1/2)
+}
+
+function trignometery(number) {
+    if(sin) {
+        return Math.sin(number)
+    } else if(cos){
+        return Math.cos(number)
+    } else if(tan){
+        return Math.tan(number)
+    }
+}*/
+
 
 const showNumbers = document.querySelector("#show-numbers")
 const getNumber = document.querySelectorAll("#num")
